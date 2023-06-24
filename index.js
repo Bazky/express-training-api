3877478259ea55f02d6cd80a6c51c9ab7581249c
Adrian Nastał <81535800+Bazky@users.noreply.github.com>
2023-06-24 14:24:31 +0200

    POST

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-async function fetchTrains(data) {
+async function fetchTrains() {
   try {
-    const response = await fetch("./trains.json");
-    return response.json();
+    const response = fetch("./trains.json", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: "John" }),
+    });
+    const result = await response.json();
+    console.log("Success:", result);
   } catch (error) {
-    console.log("Error");
+    console.error("Error:", error);
   }
 }
+
 app.get("/trains", (req, res) => {
   res.send(fetchTrains());
   res.status(500).send("Error fetching trains data");
